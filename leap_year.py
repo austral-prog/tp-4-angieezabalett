df3e8c6bc21ef3ae3bfb10338e0adee0759c915b
Angelina Zabalett <116826508+angieezabalett@users.noreply.github.com>
2024-04-17 21:15:00 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    año = int(input ("Ingrese un año: "))
+        if año % 4 == 0 or año % 400 == 0:
+            print ("El año es bisiesto")
+        else:
+            print ("El año no es bisiesto")
